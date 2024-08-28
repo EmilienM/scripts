cd5fbe593604f1655dde8e63e7a4630150fa3a72
Emilien Macchi <emacchi@redhat.com>
2024-08-28 09:02:05 -0400

    Add aws-secret.sh

---
+++ aws-secret.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+export KUBECONFIG=~/ocp/auth/kubeconfig
+TMP_DIR=$(mktemp -d)
+aws-saml.py --target-role 637423598313-route53admin
+
+cat <<EOF > $TMP_DIR/aws-credentials.yaml
+apiVersion: v1
+kind: Secret
+metadata:
+  name: aws-access-key
+  namespace: external-dns-operator
+stringData:
+    credentials: |-
+$(cat ~/.aws/credentials | sed 's/^/      /')
+EOF
+sed -i 's/saml/default/g' $TMP_DIR/aws-credentials.yaml
+
+kubectl apply -f $TMP_DIR/aws-credentials.yaml
